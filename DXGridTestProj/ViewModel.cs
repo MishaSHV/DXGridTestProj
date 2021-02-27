43557ddc3e0e3cceade79dfbb9d489900ba4210b
MishaSHV <origami690@gmail.com>
2021-02-27 14:28:38 +0200

    Added data source and bind to table

---
+++ DXGridTestProj/ViewModel.cs
@@ -0,0 +1,57 @@
+﻿using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DXGridTestProj
+{
+    class ViewModel : ViewModelBase
+    {
+        private NorthwindEntities northwindDbContext;
+
+        public ViewModel()
+        {
+            if (IsInDesignMode)
+            {
+                Orders = new ObservableCollection<Order>();
+                Shippers = new ObservableCollection<Shipper>();
+                Employees = new ObservableCollection<Employee>();
+            }
+            else
+            {
+                northwindDbContext = new NorthwindEntities();
+
+                northwindDbContext.Orders.Load();
+                Orders = northwindDbContext.Orders.Local;
+
+                northwindDbContext.Shippers.Load();
+                Shippers = northwindDbContext.Shippers.Local;
+
+                northwindDbContext.Employees.Load();
+                Employees = northwindDbContext.Employees.Local;
+            }
+        }
+
+        public ObservableCollection<Order> Orders
+        {
+            get => GetValue<ObservableCollection<Order>>();
+            private set => SetValue(value);
+        }
+
+        public ObservableCollection<Shipper> Shippers
+        {
+            get => GetValue<ObservableCollection<Shipper>>();
+            private set => SetValue(value);
+        }
+
+        public ObservableCollection<Employee> Employees
+        {
+            get => GetValue<ObservableCollection<Employee>>();
+            private set => SetValue(value);
+        }
+    }
+}
